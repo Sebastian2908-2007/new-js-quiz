246a3561c369929413d5c119ea9e129c43e6176c
Sebabin <sebastiancrespin279@gmail.com>
2021-11-09 19:27:21 -0700

    end day commit

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var minutesEl = document.querySelector(".timer__part--minutes");
+var secondsEl = document.querySelector(".timer__part--seconds");
+var startBtnEl = document.querySelector(".start");
+
+startBtnEl.addEventListener("click",()=>{});
