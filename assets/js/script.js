dd8e9b8e35582fc1b60b43be80291855a09f9af7
Sebabin <sebastiancrespin279@gmail.com>
2021-11-10 19:32:05 -0700

    now have timer functioning as a timer and visibly counting down

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,16 @@ var startBtnEl = document.querySelector(".start");
 // interval and remaining secoonds variables for timer updating
 var interval = null;
 
-var buttonUpdate = function () {   
+var buttonUpdate = function (remainingSeconds) {   
+    
+    // variable for generating one minute with division
+    var minutes = Math.floor(remainingSeconds / 60);
+    //variable for remaing seconds
+    var seconds = remainingSeconds % 60;
+        // give minutes element textContent of minutes variable as well as putting a zero in front
+   minutesEl.textContent =  minutes.toString().padStart(2, "0");
+   secondsEl.textContent = seconds.toString().padStart(2, "0");
+
     // if statement for checking wether or not interval var is null to display the correct text in button
     if (interval === null) {
      startBtnEl.textContent = "Start Quiz";
@@ -20,21 +29,24 @@ var buttonUpdate = function () {
 // function for updating the clock (timer)
 var start = function () {
     var remainingSeconds = 90;
-   // variable for generating one minute with division
-   var minutes = Math.floor(remainingSeconds / 60);
-   //variable for remaing seconds
-   var seconds = remainingSeconds % 60;
-    
-   // give minutes element textContent of minutes variable as well as putting a zero in front
+    // variable for generating one minute with division
+    var minutes = Math.floor(remainingSeconds / 60);
+    //variable for remaing seconds
+    var seconds = remainingSeconds % 60;
+        // give minutes element textContent of minutes variable as well as putting a zero in front
    minutesEl.textContent =  minutes.toString().padStart(2, "0");
-   secondsEl.textContent = seconds.toString().padStart(2 , "0");
+   secondsEl.textContent = seconds.toString().padStart(2, "0");
+  
 
 if (remainingSeconds === 0) { 
      return
   }else {
-      interval = setInterval(() => {
+      interval = setInterval(() => { 
+
       remainingSeconds--;
-      buttonUpdate();
+      
+   
+      buttonUpdate(remainingSeconds);
       console.log(remainingSeconds);
       if (remainingSeconds === 0) {
           stop();
